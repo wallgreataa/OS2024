e25caaa1525262efb52746a39ec5e1c9c2f6ff00
mu_mian <103624906+wallgreataa@users.noreply.github.com>
2024-12-03 14:29:41 +0800

    Update default_pmm.c

--- lab4/kern/mm/default_pmm.c
+++ lab4/kern/mm/default_pmm.c
@@ -9,7 +9,7 @@
    usually split, and the remainder added to the list as another free block.
    Please see Page 196~198, Section 8.2 of Yan Wei Min's chinese book "Data Structure -- C programming language"
 */
-// LAB2 EXERCISE 1: YOUR CODE
+// LAB2 EXERCISE 1: 2213912
 // you should rewrite functions: default_init,default_init_memmap,default_alloc_pages, default_free_pages.
 /*
  * Details of FFMA
@@ -229,7 +229,7 @@ basic_check(void) {
     free_page(p2);
 }
 
-// LAB2: below code is used to check the first fit allocation algorithm (your EXERCISE 1) 
+// LAB2: below code is used to check the first fit allocation algorithm
 // NOTICE: You SHOULD NOT CHANGE basic_check, default_check functions!
 static void
 default_check(void) {
@@ -293,7 +293,7 @@ default_check(void) {
     assert(count == 0);
     assert(total == 0);
 }
-
+//这个结构体在
 const struct pmm_manager default_pmm_manager = {
     .name = "default_pmm_manager",
     .init = default_init,
@@ -303,4 +303,3 @@ const struct pmm_manager default_pmm_manager = {
     .nr_free_pages = default_nr_free_pages,
     .check = default_check,
 };
-
